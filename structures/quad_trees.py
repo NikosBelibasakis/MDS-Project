9e662ad18c6f911fb2b915402540aef50e42c0bf
mira <miraslamaj@gmail.com>
2024-02-12 02:02:57 +0200

    quads structure done, now needs searching

--- structures/quad_trees.py
+++ structures/quad_trees.py
@@ -25,9 +25,9 @@ class Point3D:
         return f"Point3D(x={self.x}, y={self.y}, z={self.z})"
 
 class OctreeNode:
-    def __init__(self, position, education_text=None):
+    def __init__(self, position, education):
         self.position = position                #contains x,y,z point
-        self.education_text = education_text    #information each point holds
+        self.education = education    #information each point holds
 
 
 class Octree:
@@ -43,7 +43,7 @@ class Octree:
         
         # Current octuple cannot contain it
         if not self.top_boundary <= node.position <= self.bottom_boundary:
-            print("Node you are trying to insert is out of bounds! Node: ", node.pos)    
+            print("Node you are trying to insert is out of bounds! Node: ", node.position)    
             return
 
         # We cannot subdivide this octuple further, so the node will be a leaf node
@@ -56,9 +56,9 @@ class Octree:
         midy = (self.top_boundary.y + self.bottom_boundary.y) / 2
         midz = (self.top_boundary.z + self.bottom_boundary.z) / 2
 
-        if midx >= node.pos.x:
-            if midy >= node.pos.y:
-                if midz >= node.pos.z:
+        if midx >= node.position.x:
+            if midy >= node.position.y:
+                if midz >= node.position.z:
                     if self.children[0] is None:
                         self.children[0] = Octree(self.top_boundary, Point3D(midx, midy, midz))
                     self.children[0].insert(node)
@@ -67,7 +67,7 @@ class Octree:
                         self.children[1] = Octree(Point3D(self.top_boundary.x, self.top_boundary.y, midz), Point3D(midx, midy, self.bottom_boundary.z))
                     self.children[1].insert(node)
             else:
-                if midz >= node.pos.z:
+                if midz >= node.position.z:
                     if self.children[2] is None:
                         self.children[2] = Octree(Point3D(self.top_boundary.x, midy, self.top_boundary.z), Point3D(midx, self.bottom_boundary.y, midz))
                     self.children[2].insert(node)
@@ -76,8 +76,8 @@ class Octree:
                         self.children[3] = Octree(Point3D(self.top_boundary.x, midy, midz), Point3D(midx, self.bottom_boundary.y, self.bottom_boundary.z))
                     self.children[3].insert(node)
         else:
-            if midy >= node.pos.y:
-                if midz >= node.pos.z:
+            if midy >= node.position.y:
+                if midz >= node.position.z:
                     if self.children[4] is None:
                         self.children[4] = Octree(Point3D(midx, self.top_boundary.y, self.top_boundary.z), Point3D(self.bottom_boundary.x, midy, midz))
                     self.children[4].insert(node)
@@ -86,7 +86,7 @@ class Octree:
                         self.children[5] = Octree(Point3D(midx, self.top_boundary.y, midz), Point3D(self.bottom_boundary.x, midy, self.bottom_boundary.z))
                     self.children[5].insert(node)
             else:
-                if midz >= node.pos.z:
+                if midz >= node.position.z:
                     if self.children[6] is None:
                         self.children[6] = Octree(Point3D(midx, midy, self.top_boundary.z), Point3D(self.bottom_boundary.x, self.bottom_boundary.y, midz))
                     self.children[6].insert(node)
@@ -146,15 +146,15 @@ class Octree:
                     return self.children[7].search(p)
     
 
-# Get data from the JSON file
+# Get education from the JSON file
 with open('../scientist_info.json', 'r', encoding="utf-8") as file:
     data = json.load(file)
 
-surnames=assing_index_surname(data[:10])
+surnames=assing_index_surname(data)
 surname_ids=[id[1] for id in surnames]
-awards = [int(scientist['awards']) for scientist in data[:10]]
-dblp_record = [int(scientist['dblp_record']) for scientist in data[:10]]
-education = [scientist['education'] for scientist in data[:10]]
+awards = [int(scientist['awards']) for scientist in data]
+dblp_record = [int(scientist['dblp_record']) for scientist in data]
+education = [scientist['education'] for scientist in data]
 
 # Extract min and max values --- they will be the boundaries
 top_boundary=Point3D( min(surname_ids), min(awards) , min(dblp_record) )
@@ -165,11 +165,10 @@ print(bottom_boundary)
 
 octree = Octree(top_boundary, bottom_boundary)  #initialization of octree
 
-for i in len(surname_ids):
+for i in range(len(surname_ids)):
     node=OctreeNode(Point3D(surname_ids[i],awards[i],dblp_record[i]),education[i])
     octree.insert(node) #populate the tree with all our information
 
 #example searching
-print("Node a:", octree.search(Point3D(1, 1, 1)).data)
-print("Node b:", octree.search(Point3D(2, 5, 3)).data)
-print("Node c:", octree.search(Point3D(7, 6, 4)).data)
+print("Node a:", octree.search(Point3D(1, 10, 60)).education)
+
