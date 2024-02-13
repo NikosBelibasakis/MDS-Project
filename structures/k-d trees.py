6f78880929001654d3642ba6fe4f213857f27903
Nick Belibasakis <nikosbelibasakis71@gmail.com>
2024-02-14 00:29:09 +0200

    First version of the k-d tree structure.
     This code fetches the data from the JSON file. It also contains a NODE constructor and a function for creating a node for the k(3)-d tree.

--- structures/k-d trees.py
+++ structures/k-d trees.py
@@ -1,86 +1,55 @@
-import hashlib
+#version 1
+
+
 import json
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
-class kdTree:
-    def __init__(self, P, d=0, idx=0):
-        n = len(P)
-        m = n // 2
-        P.sort(key=lambda x: x[d])
-        self.point = P[m]
-        self.d = d
-        self.idx = idx  # Store the index
-        d = (d + 1) % len(P[0])
-        if m > 0:
-            self.left = kdTree(P[:m], d, idx)
-        if n - (m + 1) > 0:
-            self.right = kdTree(P[m + 1:], d, idx + m + 1)
+class Node:
+    # Constructor to create a new node
+    def __init__(self, surname, num_awards , num_dblp_record):
+        self.surname = surname
+        self.num_awards = num_awards
+        self.num_dblp_record = num_dblp_record
+        self.left = None
+        self.right = None
 
-        def inbox(p, box):
-            return all(min <= pi <= max for pi, (min, max) in zip(p, box))
 
-# Load data from JSON file
-with open('scientist_info.json', 'r', encoding="utf-8") as file:
+# A method to create a node of K D tree
+def newNode(surname,num_awards,num_dblp_record):
+    return Node(surname,num_awards,num_dblp_record)
+
+
+
+
+# A function to insert a new node with the given keys (surname,awards,dblp_record) in the 3-D tree
+
+
+
+
+
+
+
+
+# Get the data from the JSON file
+with open('../scientist_info.json', 'r', encoding="utf-8") as file:
     data = json.load(file)
+# Sort the data based on the "surname" key
+sorted_data = sorted(data, key=lambda x: x.get("surname", ""))
 
-# Function to get the features for the index
-def get_features(scientist_data):
-    surname = scientist_data['surname']
-    awards = scientist_data['awards']
-    dblp_record = scientist_data['dblp_record']
+#fetch the surnames
+surnames = [scientist['surname'] for scientist in data]
 
-    # Calculate hashes
-    surname_hash = int(hashlib.sha256(surname.encode('utf-8')).hexdigest(), 16)
-    awards_hash = int(hashlib.sha256(str(awards).encode('utf-8')).hexdigest(), 16)
-    dblp_record_hash = int(hashlib.sha256(str(dblp_record).encode('utf-8')).hexdigest(), 16)
+#fetch the number of awards and convert the integer into a string
+awards_int = [scientist['awards'] for scientist in data]
+awards = [str(aw) for aw in awards_int]
 
-    return [surname_hash, awards_hash, dblp_record_hash]
+#fetch the dblp record and convert the integer into a string
+dblp_int = [scientist['dblp_record'] for scientist in data]
+dblp = [str(db) for db in dblp_int]
 
-# Construct feature vectors for all scientists
-values = [get_features(scientist_data) for scientist_data in data]
+#fetch the education
+education = [scientist['education'] for scientist in data]
 
-# Build a KD-tree using kdTree class
-kdtree = kdTree(values)
 
-# Query scientist index
-get_scientist_index = 0
-get_scientist_data = data[get_scientist_index]
-get_features = get_features(get_scientist_data)
 
-# Perform range search using kdTree
-results = [(idx, kdtree.point) for idx in range(len(data))]
 
-# Function to check similarity at the education feature
-def similar_education(edu_text1, edu_text2):
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform([edu_text1, edu_text2])
-    vec_1, vec_2 = tfidf_matrix[0], tfidf_matrix[1]
 
-    similarity = cosine_similarity(vec_1, vec_2)
-    return similarity > 0.5
-
-# Getting the final results
-final_results = []
-
-for idx, _ in results:
-    scientist = data[idx]
-    surname = scientist['surname']
-    awards = scientist['awards']
-    dblp_record = scientist['dblp_record']
-
-    # Example: Check if the surname starts with a letter from 'A' to 'G' and has >4 awards and dblp_record is between [100,200]
-    if 'A' <= surname[0].upper() <= 'G' and awards > 4 and 100 <= dblp_record <= 200 :
-        # Check the similarity between the current education and the query education
-        if similar_education(get_scientist_data['education'], scientist['education']):
-            # Append the scientist data meeting the condition to final_results[]
-            final_results.append(scientist)
-        # Print the details of the scientists that meet the condition at the if statement
-        print({surname}, {awards}, {scientist['education']}, {dblp_record})
-
-        # Print the results
-        print("Surname:", scientist['surname'])
-        print("Awards:", scientist['awards'])
-        print("Education:", scientist['education'])
-        print("DBLP Record:", scientist['dblp_record'])
-        print("\n" + "=" * 30 + "\n")  # make better looking results
