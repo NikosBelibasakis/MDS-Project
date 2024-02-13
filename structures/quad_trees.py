cec6f5a651ed782132bbacaf9a5c223e820dd982
mira <miraslamaj@gmail.com>
2024-02-14 00:45:51 +0200

    quad trees almost done

--- structures/quad_trees.py
+++ structures/quad_trees.py
@@ -1,7 +1,12 @@
 import json
-from general_functions import assing_index_surname, get_features
+from general_functions import assing_index_surname
 
-"""At this point we will define  Octrees """
+"""
+At this point we will define  Octrees 
+Octrees are the equivalent of quad trees
+in three dimensional space
+"""
+
 class Point3D:
     def __init__(self, x, y, z):
         self.x = x  #surname id number
@@ -21,13 +26,17 @@ class Point3D:
     def __ge__(self, other): #greater then or equal to
         return self.x >= other.x and self.y >= other.y and self.z >= other.z
 
-    def __str__(self): #string represantion of point (not really needed)
-        return f"Point3D(x={self.x}, y={self.y}, z={self.z})"
+    def __str__(self): #string represantion of point
+        return f"Point3D(surname id={self.x}, awards={self.y}, DBLP record={self.z})"
 
 class OctreeNode:
     def __init__(self, position, education):
-        self.position = position                #contains x,y,z point
+        self.position = position      #contains x,y,z point
         self.education = education    #information each point holds
+    
+    def __str__(self):
+        return f"Scientist: \n{self.position}, \neducation={self.education}"
+        #you need to find a way to change the represantation id to the surname string
 
 
 class Octree:
@@ -58,7 +67,7 @@ class Octree:
         self.children[child_index].insert(node)
 
 
-    def search(self, p):
+    def search(self, p): #this is for exact search 
         if not self.top_boundary <= p <= self.bottom_boundary:
             return None
 
@@ -69,6 +78,25 @@ class Octree:
         if self.children[child_index] is None:
             return None
         return self.children[child_index].search(p)
+
+    def search_in_range(self,letters_id,award_threshold,records):
+        #we will find first the boundaries in which we are searching 
+
+        letters_difference=letters_id[1]-letters_id[0]
+        awards_difference=bottom_boundary.y-award_threshold
+        records_difference=records[1]-records[0]
+
+        scientist_list=[]
+
+        for i in range(letters_difference+1):
+            for j in range(awards_difference+1):
+                for k in range(records_difference+1):
+                    point=Point3D(letters_id[0]+i,award_threshold+j,records[0]+k)
+                    scientist=self.search(point)
+                    if scientist is not None:
+                        scientist_list.append(scientist)
+        return scientist_list
+
 
     def get_child(self,point):
         
@@ -113,35 +141,11 @@ class Octree:
                                 #we are at the eighth child
                                 return 7, (Point3D(midx, midy, midz), self.bottom_boundary)
 
-# Get data from the JSON file
-with open('../scientist_info.json', 'r', encoding="utf-8") as file:
-    data = json.load(file)
-
-# Sort the data based on the "surname" key
-sorted_data = sorted(data, key=lambda x: x.get("surname", ""))
-
-surnames=assing_index_surname(sorted_data)
-surname_ids=[id[1] for id in surnames]
-awards = [int(scientist['awards']) for scientist in sorted_data]
-dblp_record = [int(scientist['dblp_record']) for scientist in sorted_data]
-education = [scientist['education'] for scientist in sorted_data]
-
-# Extract min and max values --- they will be the boundaries
-top_boundary=Point3D( min(surname_ids), min(awards) , min(dblp_record) )
-bottom_boundary=Point3D( max(surname_ids) , max(awards) , max(dblp_record) )
-
-print(top_boundary)
-print(bottom_boundary)
-
-octree = Octree(top_boundary, bottom_boundary)  #initialization of octree
-
-for i in range(len(surname_ids)):
-    node=OctreeNode(Point3D(surname_ids[i],awards[i],dblp_record[i]),education[i])
-    octree.insert(node) #populate the tree with all our information
-
-#example searching
-print("Node a:", octree.search(Point3D(1, 0, 1119)).education)
-
+"""
+Here we will describe some functions that are going to help
+with the searching of the Octree 
+"""
+#It generates the ranges of the ids for each first letter that surnames have
 def letter_id_range(surnames):
     start_id=surnames[0][1]
     current_letter=''
@@ -166,4 +170,47 @@ def letter_id_range(surnames):
     
     return letters_ranges
 
-x=letter_id_range(surnames)
+
+"""
+From now on we describe the main routine
+"""
+
+# Get data from the JSON file
+with open('../scientist_info.json', 'r', encoding="utf-8") as file:
+    data = json.load(file)
+
+# Sort the data based on the "surname" key so that they are from A to Z
+sorted_data = sorted(data, key=lambda x: x.get("surname", ""))
+
+surnames=assing_index_surname(sorted_data)
+surname_ids=[id[1] for id in surnames]
+awards = [int(scientist['awards']) for scientist in sorted_data]
+dblp_record = [int(scientist['dblp_record']) for scientist in sorted_data]
+education = [scientist['education'] for scientist in sorted_data]
+
+# Extract min and max values --- they will be the boundaries
+top_boundary=Point3D( min(surname_ids), min(awards) , min(dblp_record) )
+bottom_boundary=Point3D( max(surname_ids) , max(awards) , max(dblp_record) )
+
+print(top_boundary)
+print(bottom_boundary)
+
+octree = Octree(top_boundary, bottom_boundary)  #initialization of octree
+
+for i in range(len(surname_ids)):
+    node=OctreeNode(Point3D(surname_ids[i],awards[i],dblp_record[i]),education[i])
+    octree.insert(node) #populate the tree with all our information
+
+letter_ranges=letter_id_range(surnames)
+
+range_a=letter_ranges.get("Z")[0]
+range_g=letter_ranges.get("Z")[1] #use .upper() later
+
+#example searching
+print("Node a:", octree.search(Point3D(1, 0, 1119)).education)
+
+letters=[range_a,range_g]
+
+scientists=octree.search_in_range(letters,0,[4,118])
+for i in range(len(scientists)):
+     print(scientists[i])
